e1ef7eae7e162959643da8479b83e202c996c9c6
elnatnael <meranat9104@gmail.com>
2025-11-03 08:48:04 +0300

    Create a User Profile Card Using Props

--- alx-react-app/src/components/UserProfile.jsx
+++ alx-react-app/src/components/UserProfile.jsx
@@ -3,7 +3,7 @@ function UserProfile(props) {
     <div className="user-profile">
       <h2>{props.name}</h2>
       <p>Age: {props.age}</p>
-      <p>{props.bio}</p>
+      <p>Bio: {props.bio}</p>
     </div>
   );
 }
